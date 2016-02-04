91f606a8b2c547b36c8efc62846faa3132ed36ed
Danny Pule <danny.pule@mippin.com>
2016-02-04 17:49:20 +0000

    connect and receive from mongo

--- server.js
+++ server.js
@@ -6,19 +6,29 @@ const MongoClient = require('mongodb').MongoClient;
 app.use(bodyParser.urlencoded({extend: true}));
 
 app.get('/', (req, res) => {
+
+	db.collection('quotes').find().toArray(function(err, results){
+		console.log(results);
+	});
 	res.sendFile(__dirname + '/index.html'); 
 });
 
 app.post('/quotes', (req, res) => {
-	console.log(req.body);
+	// console.log(req.body);
+
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+
+		console.log('saved to database');
+		res.redirect('/');
+	});
 });
 
-MongoClient.connect('ds055935.mongolab.com:55935/star_wars', (err, database) => {
+MongoClient.connect('mongodb://star_wars:rollingstones@ds055935.mongolab.com:55935/star_wars', (err, database) => {
   // ... start the server
   if (err) return console.log(err);
 
   db = database;
-
 
   app.listen(3000, () => {
   	console.log("Listening on port 3000.");
